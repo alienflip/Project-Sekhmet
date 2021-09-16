2ce54150fb5359f2874bb22566782c70ed71b06e
Kai <73109076+alienflip@users.noreply.github.com>
2021-09-16 22:50:21 +0100

    Update host.cpp

--- host.cpp
+++ host.cpp
@@ -209,7 +209,7 @@ int _tmain(int argc, TCHAR* argv[]) {
     cl_device_type deviceType = CL_DEVICE_TYPE_GPU;
 
     // arrayWidth and arrayHeight must be powers of two
-    cl_uint arrayWidth = 32;
+    cl_uint arrayWidth = 16;
     cl_uint arrayHeight = arrayWidth / 4;
     cl_int size = arrayHeight * arrayWidth;
 
