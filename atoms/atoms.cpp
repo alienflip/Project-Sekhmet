653f683e39df8acf851022dd0bd3cb88f5011ded
Kai <kharris_r@aol.com>
2021-11-02 20:01:12 +0000

    atoms

---
+++ atoms/atoms.cpp
@@ -0,0 +1,30 @@
+#include <atomic>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+void print_vec(std::vector<int>* vec ){
+    std::cout << std::endl;
+    for (auto& el : *vec) std::cout << el << " ";
+    std::cout << std::endl;
+}
+
+void mutate_vec(std::vector<int>* vec) {
+    for(auto& el : *vec) el++;
+}
+
+int pop(std::vector<int>* vec) {
+    int p = vec->back();
+    vec->pop_back();
+    return p;
+}
+
+int main(void) {
+    std::vector<int> vec;
+    for(int i = 0; i < 128; i++) vec.push_back(i);
+    print_vec(&vec);
+    mutate_vec(&vec);
+    std::cout << std::endl << pop(&vec) << std::endl;
+    print_vec(&vec);
+    return 0;
+}
