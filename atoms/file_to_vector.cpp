878aa57d9390233ec7dc0f28479afdc9a4920a7e
Kai <73109076+alienflip@users.noreply.github.com>
2021-11-09 03:54:11 +0000

    Update file_to_vector.cpp

--- atoms/file_to_vector.cpp
+++ atoms/file_to_vector.cpp
@@ -11,8 +11,8 @@ int main(void) {
 
     file.exceptions(
         std::ifstream::badbit
-    | std::ifstream::failbit
-    | std::ifstream::eofbit);
+        | std::ifstream::failbit
+        | std::ifstream::eofbit);
 
     file.open("text.txt", std::ifstream::in | std::ifstream::binary);
     file.seekg(0, std::ios::end);
@@ -25,7 +25,7 @@ int main(void) {
 
     // check output (size in bytes, elements in vector)
     std::cout << length << std::endl;
-
+    std::cout << std::endl;
     for(auto& el : vec) {
         std::cout << el;
     } std::cout << std::endl;
