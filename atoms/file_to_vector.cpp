895e16dea7065a26c634c9334d110ecf870c5b4b
Kai <73109076+alienflip@users.noreply.github.com>
2021-11-09 03:52:13 +0000

    Create file_to_vector.cpp

---
+++ atoms/file_to_vector.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+int main(void) {
+
+    // write data from file into local vector
+    std::vector<char> vec;
+
+    std::ifstream file;
+
+    file.exceptions(
+        std::ifstream::badbit
+    | std::ifstream::failbit
+    | std::ifstream::eofbit);
+
+    file.open("text.txt", std::ifstream::in | std::ifstream::binary);
+    file.seekg(0, std::ios::end);
+    std::streampos length(file.tellg());
+    if (length) {
+        file.seekg(0, std::ios::beg);
+        vec.resize(static_cast<std::size_t>(length));
+        file.read(&vec.front(), static_cast<std::size_t>(length));
+    }
+
+    // check output (size in bytes, elements in vector)
+    std::cout << length << std::endl;
+
+    for(auto& el : vec) {
+        std::cout << el;
+    } std::cout << std::endl;
+    return 0;
+}
